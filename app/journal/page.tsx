c1ed27ea1d20c03c53ae50bd7898f86e7ddcc1dc
Sean Dowdall <seanied1234567@gmail.com>
2023-09-12 12:31:24 +0200

    Added Recent Journal entries templated section

--- app/journal/page.tsx
+++ app/journal/page.tsx
@@ -1,7 +1,17 @@
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import { Textarea } from "@/components/ui/textarea";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function JournalPage() {
   return (
@@ -18,6 +28,62 @@ export default function JournalPage() {
         <div className="grid w-full gap-2 pt-5">
           <Textarea placeholder="Type your message here." />
           <Button>Add Journal Entry</Button>
+        </div>
+      </div>
+      <div className="flex flex-col space-y-2">
+        <h2 className="text-3xl font-bold tracking-tight pt-12 space-y-4">
+          Recent Journal Entries
+        </h2>
+        <div className="flex justify-center space-x-5 pb-20">
+          <div className="w-1/3">
+            <Card>
+              <CardHeader>
+                <CardTitle>11/9/23</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>
+                  Build a snake game in python! Easy lesson for beginners ...
+                </p>
+              </CardContent>
+              <CardFooter>
+                <Link href="">
+                  <Button>Try Free</Button>
+                </Link>
+              </CardFooter>
+            </Card>
+          </div>
+          <div className="w-1/3">
+            <Card>
+              <CardHeader>
+                <CardTitle>10/9/23</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>
+                  Build a snake game in python! Easy lesson for beginners ...
+                </p>
+              </CardContent>
+              <CardFooter>
+                <Link href="/courses/python-basics">
+                  <Button>Try Free</Button>
+                </Link>
+              </CardFooter>
+            </Card>
+          </div>
+          <div className="w-1/3">
+            <Card>
+              <CardHeader>
+                <CardTitle>9/9/23</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>
+                  Build a snake game in python! Easy lesson for beginners ...
+                </p>
+              </CardContent>
+              <CardFooter>
+                <Button>Try Free</Button>
+              </CardFooter>
+            </Card>
+          </div>
         </div>
       </div>
     </div>
